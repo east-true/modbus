ff54a40d762202bbb74b94febea237be30cafa80
east-true <leik24@naver.com>
2023-08-01 12:11:12 +0900

    edit send

--- rtuToTcpClient.go
+++ rtuToTcpClient.go
@@ -4,10 +4,18 @@
 
 package modbus
 
+import (
+	"io"
+	"log"
+	"net"
+	"sync"
+	"time"
+)
+
 // RtuViaTCPClientHandler implements Packager and Transporter interface.
 type RtuViaTCPClientHandler struct {
 	rtuPackager
-	tcpTransporter
+	RtuViaTCPSerialTransporter
 }
 
 // NewRtuViaTCPClientHandler allocates a new RtuViaTCPClientHandler.
@@ -23,4 +31,205 @@ func NewRtuViaTCPClientHandler(address string) *RtuViaTCPClientHandler {
 func RtuViaTCPClient(address string) Client {
 	handler := NewRtuViaTCPClientHandler(address)
 	return NewClient(handler)
+}
+
+// rtuSerialTransporter implements Transporter interface.
+type RtuViaTCPSerialTransporter struct {
+	// Connect string
+	Address string
+	// Connect & Read timeout
+	Timeout time.Duration
+	// Idle timeout to close the connection
+	IdleTimeout time.Duration
+	// Transmission logger
+	Logger *log.Logger
+
+	// TCP connection
+	mu           sync.Mutex
+	conn         net.Conn
+	closeTimer   *time.Timer
+	lastActivity time.Time
+
+	// Baud rate (default 19200)
+	BaudRate int
+	// Data bits: 5, 6, 7 or 8 (default 8)
+	DataBits int
+	// Stop bits: 1 or 2 (default 1)
+	StopBits int
+	// Parity: N - None, E - Even, O - Odd (default E)
+	// (The use of no parity requires 2 stop bits.)
+	Parity string
+}
+
+func (mb *RtuViaTCPSerialTransporter) Send(aduRequest []byte) (aduResponse []byte, err error) {
+	mb.mu.Lock()
+	defer mb.mu.Unlock()
+
+	// Establish a new connection if not connected
+	if err = mb.connect(); err != nil {
+		return
+	}
+
+	// Set timer to close when idle
+	mb.lastActivity = time.Now()
+	mb.startCloseTimer()
+
+	// Set write and read timeout
+	var timeout time.Time
+	if mb.Timeout > 0 {
+		timeout = mb.lastActivity.Add(mb.Timeout)
+	}
+	if err = mb.conn.SetDeadline(timeout); err != nil {
+		return
+	}
+
+	// Send data
+	mb.logf("modbus: sending % x", aduRequest)
+	if _, err = mb.conn.Write(aduRequest); err != nil {
+		return
+	}
+
+	function := aduRequest[1]
+	functionFail := aduRequest[1] & 0x80
+	bytesToRead := calculateResponseLength(aduRequest)
+	time.Sleep(mb.calculateDelay(len(aduRequest) + bytesToRead))
+
+	var n int
+	var n1 int
+	var data [rtuMaxSize]byte
+	//We first read the minimum length and then read either the full package
+	//or the error package, depending on the error status (byte 2 of the response)
+	if _, err = io.ReadFull(mb.conn, data[:rtuMinSize]); err != nil {
+		mb.flush(data[:])
+		return
+	}
+
+	//if the function is correct
+	if data[1] == function {
+		//we read the rest of the bytes
+		if n < bytesToRead {
+			if bytesToRead > rtuMinSize && bytesToRead <= rtuMaxSize {
+				if bytesToRead > n {
+					n1, err = io.ReadFull(mb.conn, data[n:bytesToRead])
+					n += n1
+				}
+			}
+		}
+	} else if data[1] == functionFail {
+		//for error we need to read 5 bytes
+		if n < rtuExceptionSize {
+			n1, err = io.ReadFull(mb.conn, data[n:rtuExceptionSize])
+		}
+		n += n1
+	}
+
+	if err != nil {
+		mb.flush(data[:])
+		return
+	}
+
+	aduResponse = data[:n]
+	mb.logf("modbus: received % x\n", aduResponse)
+	return
+}
+
+// Connect establishes a new connection to the address in Address.
+// Connect and Close are exported so that multiple requests can be done with one session
+func (mb *RtuViaTCPSerialTransporter) Connect() error {
+	mb.mu.Lock()
+	defer mb.mu.Unlock()
+
+	return mb.connect()
+}
+
+func (mb *RtuViaTCPSerialTransporter) connect() error {
+	if mb.conn == nil {
+		dialer := net.Dialer{Timeout: mb.Timeout}
+		conn, err := dialer.Dial("tcp", mb.Address)
+		if err != nil {
+			return err
+		}
+		mb.conn = conn
+	}
+	return nil
+}
+
+func (mb *RtuViaTCPSerialTransporter) startCloseTimer() {
+	if mb.IdleTimeout <= 0 {
+		return
+	}
+	if mb.closeTimer == nil {
+		mb.closeTimer = time.AfterFunc(mb.IdleTimeout, mb.closeIdle)
+	} else {
+		mb.closeTimer.Reset(mb.IdleTimeout)
+	}
+}
+
+// Close closes current connection.
+func (mb *RtuViaTCPSerialTransporter) Close() error {
+	mb.mu.Lock()
+	defer mb.mu.Unlock()
+
+	return mb.close()
+}
+
+// flush flushes pending data in the connection,
+// returns io.EOF if connection is closed.
+func (mb *RtuViaTCPSerialTransporter) flush(b []byte) (err error) {
+	if err = mb.conn.SetReadDeadline(time.Now()); err != nil {
+		return
+	}
+	// Timeout setting will be reset when reading
+	if _, err = mb.conn.Read(b); err != nil {
+		// Ignore timeout error
+		if netError, ok := err.(net.Error); ok && netError.Timeout() {
+			err = nil
+		}
+	}
+	return
+}
+
+func (mb *RtuViaTCPSerialTransporter) logf(format string, v ...interface{}) {
+	if mb.Logger != nil {
+		mb.Logger.Printf(format, v...)
+	}
+}
+
+// closeLocked closes current connection. Caller must hold the mutex before calling this method.
+func (mb *RtuViaTCPSerialTransporter) close() (err error) {
+	if mb.conn != nil {
+		err = mb.conn.Close()
+		mb.conn = nil
+	}
+	return
+}
+
+// closeIdle closes the connection if last activity is passed behind IdleTimeout.
+func (mb *RtuViaTCPSerialTransporter) closeIdle() {
+	mb.mu.Lock()
+	defer mb.mu.Unlock()
+
+	if mb.IdleTimeout <= 0 {
+		return
+	}
+	idle := time.Now().Sub(mb.lastActivity)
+	if idle >= mb.IdleTimeout {
+		mb.logf("modbus: closing connection due to idle timeout: %v", idle)
+		mb.close()
+	}
+}
+
+// calculateDelay roughly calculates time needed for the next frame.
+// See MODBUS over Serial Line - Specification and Implementation Guide (page 13).
+func (mb *RtuViaTCPSerialTransporter) calculateDelay(chars int) time.Duration {
+	var characterDelay, frameDelay int // us
+
+	if mb.BaudRate <= 0 || mb.BaudRate > 19200 {
+		characterDelay = 750
+		frameDelay = 1750
+	} else {
+		characterDelay = 15000000 / mb.BaudRate
+		frameDelay = 35000000 / mb.BaudRate
+	}
+	return time.Duration(characterDelay*chars+frameDelay) * time.Microsecond
 }
